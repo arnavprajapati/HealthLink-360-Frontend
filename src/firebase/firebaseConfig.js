bd396c0e42cf62d67b36532eb76a10369409532d
Arnav <arnavprajapati3101@gmail.com>
2025-11-25 21:47:31 +0530

    Add .env to gitignore

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,17 +3,17 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBhsatK_uAGzSrw4QQAup1NYiniuJ1Qkl0",
-  authDomain: "healthlink-360.firebaseapp.com",
-  projectId: "healthlink-360",
-  storageBucket: "healthlink-360.firebasestorage.app",
-  messagingSenderId: "477790303180",
-  appId: "1:477790303180:web:501b5c4aa968ab7d10a369",
-  measurementId: "G-3C6HWHQFT5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 
