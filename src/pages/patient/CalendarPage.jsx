082f36d24452d3a1d950fde7c9e88aed2fcbfb14
Arnav <arnavprajapati3101@gmail.com>
2025-12-22 10:39:25 +0530

    .

--- src/pages/patient/CalendarPage.jsx
+++ src/pages/patient/CalendarPage.jsx
@@ -20,7 +20,7 @@ import {
     Minus
 } from 'lucide-react';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL 
 axios.defaults.withCredentials = true;
 
 const CalendarPage = () => {
